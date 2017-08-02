2a2bc1de78b840a5838c8f4c9f216ccee589af3a
Kate Bohuslavska <belikeme103@gmail.com>
2017-08-02 17:58:47 +0300

    videolist dislpaying

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './searchBar';
+import VideoList from './videoList';
+
+const API_KEY ='AIzaSyCuquztkNr-iQXzkCR_5DI20FMjTnAoAnU';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'music'}, (videos) => {
+      this.setState({ videos });
+      //what means {videos: videos}
+    });
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <center>That is our App</center>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
     );
   }
 }
---
+++ src/components/searchBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state ={term:''};
+    }
+    
+    onInputChange(e){
+        this.setState({ term: e.target.value });
+    }
+
+    render(){
+        return (
+            <div>
+                <input 
+                value={this.state.term}
+                onChange={this.onInputChange.bind(this)} />
+                <br/>
+                Value of the input: {this.state.term}
+            </div>
+        );
+    }
+};
+
+export default SearchBar;
